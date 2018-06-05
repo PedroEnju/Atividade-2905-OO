7ef25be50d52b2d877ad82e75fe8ea4f318b447d
Pedro Enju <pedro_enju@hotmail.com>
2018-06-05 19:57:52 -0300

    Adicionado ISBN e renomeado um método na classe Atividade2905. part III

--- br/com/pedroenju/Atividade2905.java
+++ br/com/pedroenju/Atividade2905.java
@@ -11,11 +11,11 @@ public class Atividade2905 {
     public static void main(String[] args) {
         livraria = new LivroDeLivraria_extends[numCad];
         biblioteca = new LivroDeBiblioteca_extends[numCad];
-        
+
         for (int i = 0; i < numCad; i++) {
             livraria[i] = new LivroDeLivraria_extends();
             biblioteca[i] = new LivroDeBiblioteca_extends();
-            cadastroPadrao(i);
+            cadastroManual(i);
         }
 
         for (int i = 0; i < numCad; i++) {
@@ -27,10 +27,10 @@ public class Atividade2905 {
             livraria[i].setValor(Float.parseFloat(JOptionPane.showInputDialog(
                     "VALOR em R$\r\n" + biblioteca[i].getTitulo())));
         }
-        
+
         imprimirBiblioteca();
         imprimirLivraria();
-        
+
     }
 
     public static void imprimirBiblioteca() {
@@ -51,16 +51,37 @@ public class Atividade2905 {
             System.out.println("Ano de Publicação.: " + l.getAnoPublicacao());
             System.out.println("Páginas.: " + l.getNumPagina());
             System.out.println("Editora.: " + l.getEditora());
+            System.out.println("ISBN.: " + l.getIsbn());
             System.out.println("Valor.: R$" + l.getValor() + "\r\n");
         }
     }
 
-    public static void cadastroPadrao(int num) {
-        String[] titulo = {"The Hitchhiker's Guide to the Galaxy", "As Aventuras de Sherlock Holmes", "A Pirâmide Vermelha"};
-        int[] ano = {1979, 1892, 2010};
-        String[] autor = {"Douglas Adams", "Arthur Conan Doyle", "Rick Riordan"};
-        String[] editora = {"Pan Books", "George Newnes", "Disney Publishing Worldwide"};
-        int[] numPagina = {224, 416, 516};
+    public static void cadastroManual(int num) {
+        String[] titulo = {
+            "The Hitchhiker's Guide to the Galaxy",
+            "As Aventuras de Sherlock Holmes",
+            "A Pirâmide Vermelha"};
+        int[] ano = {
+            1979,
+            1892,
+            2010};
+        String[] autor = {
+            "Douglas Adams",
+            "Arthur Conan Doyle",
+            "Rick Riordan"};
+        String[] editora = {
+            "Pan Books",
+            "George Newnes",
+            "Disney Publishing Worldwide"};
+        int[] numPagina = {
+            224,
+            416,
+            516};
+
+        String[] isbn = {
+            "9812371813",
+            "9083192387",
+            "9731923816"}; //Esses valores são meramente ilustrativos
 
         if (num < 3) {
             biblioteca[num].setTitulo(titulo[num]);
@@ -74,6 +95,7 @@ public class Atividade2905 {
             livraria[num].setAutor(autor[num]);
             livraria[num].setEditora(editora[num]);
             livraria[num].setNumPagina(numPagina[num]);
+            livraria[num].setIsbn(isbn[num]);
         }
     }
 
